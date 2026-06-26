27b51fecd612bed5fbb10a3a6becf9d4ac01ca2c
Muhammad Ahmad Khan <mahmad.khan062006@gmail.com>
2026-06-26 12:25:47 +0500

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,7 @@ import mdx from '@astrojs/mdx';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  site: 'https://ghostacoffee.github.io',
+  site: 'https://mahmadkhan.is-a.dev',
   integrations: [
     mdx(),
     tailwind({ applyBaseStyles: false }),
